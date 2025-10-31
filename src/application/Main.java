2235252fa9bbbc63efdb37637ca241ae9d2a42b4
Natan <nr020775@gmail.com>
2025-10-31 15:05:01 -0300

    feat(chess): add methods for moving pieces

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,12 +1,29 @@
 package application;
 
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
-        UI.printBoard(chessMatch.getPiece());
+        while (true) {
+            UI.printBoard(chessMatch.getPiece());
+            System.out.println();
+            System.out.print("Source: ");
+            ChessPosition source = UI.ReadChessPosition(sc);
+
+            System.out.println();
+            System.out.print("Target: ");
+            ChessPosition target = UI.ReadChessPosition(sc);
+
+            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+        }
+
 
     }
 }
